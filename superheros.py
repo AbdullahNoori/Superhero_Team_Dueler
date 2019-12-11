643429f43b5b10480c48f59aa0450a0edf29d20e
AbdullahNoori <51065314+AbdullahNoori@users.noreply.github.com>
2019-12-10 19:09:26 -0800

    updates finalized

--- superheros.py
+++ superheros.py
@@ -133,13 +133,13 @@ class Team:
         self.heroes.append(hero)
 
     def stat(self):
-        '''Print team statistics'''
+        # '''Print team statistics'''
         for hero in self.heroes:
-        print("Your hero{}:".format(hero.name))
-        print("{}" Kill/Deaths: {}.format(hero.name, hero.deaths))
+            print("Your hero:{}".format(hero.name))
+            print("Kill/Deaths: {}/{}".format(hero.name, hero.deaths))
 
     def revive_heroes(self):
-    ''' Reset all heroes health to starting_health'''
+        # ''' Reset all heroes health to starting_health'''
         for hero in self.heroes:
             hero.current_health = hero.starting_health
 
@@ -154,14 +154,15 @@ class Team:
         for hero in other_team.heroes:
             living_oppenents.append(hero)
 
-        While len(living_heroes) > 0 and len(living_oppenents) > 0:
+        while len(living_heroes) > 0 and len(living_oppenents) > 0:
             random_hero = hero.choice(living_heroes)
             random_opponent = random.choice(living_heroes)
             random_hero.fight(random_oppenent)
-
+            
             if random_hero.is_alive():
                 living_oppenents.remove(random_oppenents)
-            else: living_heroes.remove(random_hero)
+            else: 
+                living_heroes.remove(random_hero)
 
     def currently_alive(self):
         currently_alive = []
@@ -176,85 +177,81 @@ class Team:
             # 3) update the list of living_heroes and living_opponents
             # to reflect the result of the fight
 class Arena:
-    def __init__(self, team_one, team_two):
-            '''Instantiate properties'''
-            self.team_one = team_one
-            self.team_two = team_two
-            self.team_Looney_Tune = input( "What is the name of the first team?")
-            self.team_Disney = input("What is the name of the second team?")
-            self.team_one = Team(self.team_Looney_Tune)
-            self.team_two = Team(self.team_Disney)
+    def __init__(self):
+        # '''Instantiate properties'''
+        print("\nWelcome to the Champions!'\n")
+        self.team_Looney_Tune = input( "What is the name of the first team?")
+        self.team_Disney = input("What is the name of the second team?")
+        self.team_one = Team(self.team_Looney_Tune)
+        self.team_two = Team(self.team_Disney)
     
     def create_ability(self):
-        '''Prompt for Ability information.
-        return Abillity with values from user Input'''
+        # '''Prompt for Ability information.
+        # return Abillity with values from user Input'''
         name = input("What is the ability name?")
-        max_damage = input("What is the max of the ability?')
+        max_damage = input("What is the max of the ability?")
         return Ability(name, max_damage)
-        wf
+        
     def create_weapon(self):
-            '''Prompt user for Weapon information
-            return Weapon with values from user input'''
+        # '''Prompt user for Weapon information
+        # return Weapon with values from user input'''
         weapon_name = input("Create a new weapon...")
-        max_damage = input("Max Damage.")
+        max_damage = input("Max Damage?")
         return weapon(weapon_name, int(max_damage))
 
-    def create_armor(self):e
+    def create_armor(self):
         armor_name = input("Time to name your armor!")
         armor_block = input("Max Armor block")
-        return armor(armor_name, int(armor_block)
+        return armor(armor_name, int(armor_block))
 
     def create_hero(self):
-         '''Prompt user for Hero information
-          return Hero with values from user input.
-        '''
-        hero_name - input("Hero's name:")
-        hero = Hero(hero name)
-        add_item = None 
-        while add_item !+ "4":
-            add_item = input("[1] Add ability\n[2] Add weapon\n[3 Add armor\n[4] Done adding items\n\nYour choice: ")
-            if add_item == "1':
+        #Make your hero
+        hero_name = input("Hero's name: ")
+        hero = Hero(hero_name)
+        add_item = None
+        while add_item != "4":
+            add_item = input("\n[1] Add ability\n[2] Add weapon\n[3] Add armor\n[4] Done adding items\n\nYour choice: ")
+            if add_item == "1":
                 ability = self.create_ability
                 hero.add_ability(ability)
             elif add_item == "2":
                 weapon = self.create_weapon()
                 hero.add_weapon(weapon)
-            elif add_item =="3"
-                armor = self.create_weapon()
+            elif add_item == "3":
+                armor = self.create_armor()
                 hero.add_armor(armor)
         return hero
 
     def build_team_one(self):
-        '''Prompt the user to build team_one '''
-
+        # '''Prompt the user to build team_one '''
         team_Looney_Tune = input("How many Heroes are on your team?")
         
         if team_Looney_Tune != None:
             for _ in range(0, int(team_Looney_Tune)):
-                hero - self. create_hero()
+                hero = self. create_hero()
                 self.team_one.add_hero(hero)
 
-            else 
+            else:
                 team_Looney_Tune = input("Incorrect Input. Please enter a number:")
                 return team_Looney_Tune
 
-        def build_team_two(self):
+    def build_team_two(self):
             
-            team_Disney = input("Select how many hereos are you on team 2")")
+        team_Disney = input("Select how many hereos are you on team 2")
 
-            if team_Disney != None:
-                for _ in range(0, int(team_Disney)):
-                    hero = self.create_hero()
-                    self.two_add.add_hero(hero)
-                else 
-                    team_Disney = input("Sorry, incorrect input. Please enter a number:")
+        if team_Disney != None:
+            for _ in range(0, int(team_Disney)):
+                hero = self.create_hero()
+                self.two_add.add_hero(hero)
+        else: 
+            team_Disney = input("Sorry, incorrect input. Please enter a number:")
 
         def team_battle(self):
             #fight bewteen team_one and team_two
-            self. team_one.attack(self.team_two):
+            self. team_one.attack(self.team_two)
         
-        def show stats(self):
-            print ("\nStats"):
+        def show_stats(self):
+            print ("\nStats:")
             self.team_one.stats()
             self.team_two.stats()
 
@@ -263,29 +260,28 @@ class Arena:
             else:
                 print(self.team_two.name + "wins")
 
+if __name__ == "__main__":
+    game_is_running = True
 
-    if __name__ == "__main__":
-        game_is_running = True
+    # Instantiate Game Arena
+    arena = Arena()
 
-        # Instantiate Game Arena
-        arena = Arena()
+    #Build Teams
+    arena.build_team_one()
+    arena.build_team_two()
 
-        #Build Teams
-        arena.build_team_one()
-        arena.build_team_two()
+    while game_is_running:
 
-        while game_is_running:
-
-            arena.team_battle()
-            arena.show_stats()
-            play_again = input("Play Again? Y or N: ")
+        arena.team_battle()
+        arena.show_stats()
+        play_again = input("Play Again? Y or N: ")
 
             #Check for Player Input
-            if play_again.lower() == "n":
-                game_is_running = False
+        if play_again.lower() == "n":
+            game_is_running = False
 
-            else:
-                #Revive heroes to play again
-                arena.team_one.revive_heroes()
-                arena.team_two.revive_heroes()
+        else:
+            #Revive heroes to play again
+            arena.team_one.revive_heroes()
+            arena.team_two.revive_heroes()
     
